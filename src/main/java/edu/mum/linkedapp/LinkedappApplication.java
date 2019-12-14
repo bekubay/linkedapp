081a7ff082f831b2c24e4be03213af94ab0694f9
bekubay <GOmilitary2013!!>
2019-12-14 00:34:34 -0600

    added internationalization

--- src/main/java/edu/mum/linkedapp/LinkedappApplication.java
+++ src/main/java/edu/mum/linkedapp/LinkedappApplication.java
@@ -7,14 +7,19 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
 
 @SpringBootApplication
 @EnableConfigurationProperties(StorageProperties.class)
-public class LinkedappApplication {
+public class LinkedappApplication implements WebMvcConfigurer {
 
 	public static void main(String[] args) {
 			SpringApplication.run(LinkedappApplication.class, args);
@@ -26,5 +31,24 @@ public class LinkedappApplication {
 			storageService.deleteAll();
 			storageService.init();
 		};
+	}
+
+	@Bean
+	public LocaleResolver localeResolver(){
+		SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+		localeResolver.setDefaultLocale(Locale.US);
+		return localeResolver;
+	}
+
+	@Bean
+	public LocaleChangeInterceptor localeChangeInterceptor(){
+		LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+		localeChangeInterceptor.setParamName("lang");
+		return localeChangeInterceptor;
+	}
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(localeChangeInterceptor());
 	}
 }
